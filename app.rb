9efe85842a6f5e586cec58cdc77569d33f37bb64
lprates05 <lorenapratesdl@outlook.com>
2025-04-20 23:23:08 +0000

    exchange

--- app.rb
+++ app.rb
@@ -17,9 +17,25 @@ get("/") do
   
 end
 
-get ("/:first_symbol") do 
+get ("/:from_currency") do 
 
-    @the_symbol = params.fetch("first_symbol")
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("MY_KEY")}"
+  
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @currencies = @parsed_data.fetch("currencies")
+  
+  @the_symbol = params.fetch("from_currency")
 
   erb(:step_1)
+end
+
+get ("/:from_currency/:to_currency") do
+
+  @from = params.fetch ("from_currency")
+  @to = params.fetch ("to_currency")
+
+
+  erb(:step_2)
 end
